991ce51be38fc5d23a6a855011d329ad88d520fb
monimaj89 <mn.majchrowicz@gmail.com>
2023-04-29 12:03:06 +0000

    small change

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -6,6 +6,7 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
